baf4da47ec454bbcf4bfaad651e0307d31b171e9
CEEDS <carloseduardosesilva@gmail.com>
2021-02-23 11:53:30 -0300

    sixth commit

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -15,20 +15,26 @@ export class CalculatorComponent implements OnInit {
   nextOperation: string = "";
   lastChar: any = "";
 
+  operation: Array<string> = [""];
+  operationNumber: number = 0;
+
   newValue: number = 0;
   storedValue: number = 0;
 
   firstOp: boolean = true;
+  operationEnded: boolean = false;
   operationBreak: boolean = true;
+
 
   calculate(value: string) {
     if ((!isNaN(this.lastChar)) && (this.lastChar != "")) {
-
+      this.operationEnded = false;
       this.setValue();
       this.operationBreak = true;
 
       if (value === '=') {
-        this.doOperation(this.newValue, this.nextOperation)
+        this.doOperation(this.newValue, value)
+        this.operationEnded = true;
 
         if (this.newValue === 0 && this.nextOperation === "/") {
           this.displayNumber = "ERROR";
@@ -39,9 +45,8 @@ export class CalculatorComponent implements OnInit {
           this.changeSmallDisplay(" = " + this.storedValue, "operation");
         }
         this.nextOperation = value;
-
       } else {
-        this.doOperation(this.newValue, this.nextOperation)
+        this.doOperation(this.newValue, value)
 
         if (this.nextOperation === "=") {
           this.clearSmallDisplay();
@@ -55,30 +60,64 @@ export class CalculatorComponent implements OnInit {
     }
   }
 
+
   doOperation(value: number, next: string) {
-    if (this.firstOp === false) {
-      if (this.nextOperation === '+') {
-        this.storedValue = this.storedValue + this.newValue;
-      } else if
-        (this.nextOperation === '-') {
-        this.storedValue = this.storedValue - this.newValue;
-      } else if
-        (this.nextOperation === '*') {
-        this.storedValue = this.storedValue * this.newValue;
-      } else if
-        (this.nextOperation === '/') {
-        this.storedValue = this.storedValue / this.newValue;
+    if (next === "=") {
+      this.operation.push(value.toString())
+
+      while (this.operation.includes("*") || this.operation.includes("/")) {
+        for (let i = 1; i <= this.operation.length; i++) {
+          if (this.operation[i] === "*") {
+
+            this.operationNumber = Number(this.operation[i - 1]) * Number(this.operation[i + 1])
+            this.operation.splice(i - 1, 3, this.operationNumber.toString())
+            break;
+
+          } else if (this.operation[i] === "/") {
+
+            this.operationNumber = Number(this.operation[i - 1]) / Number(this.operation[i + 1])
+            this.operation.splice(i - 1, 3, this.operationNumber.toString())
+            break;
+
+          }
+        }
       }
-    } else {
-      this.storedValue = Number(this.displayNumber);
-      this.firstOp = false;
+
+      while (this.operation.includes("+") || this.operation.includes("-")) {
+        for (let i = 1; i <= this.operation.length; i++) {
+          if (this.operation[i] === "+") {
+
+            this.operationNumber = Number(this.operation[i - 1]) + Number(this.operation[i + 1])
+            this.operation.splice(i - 1, 3, this.operationNumber.toString())
+            break;
+
+          } else if (this.operation[i] === "-") {
+
+            this.operationNumber = Number(this.operation[i - 1]) - Number(this.operation[i + 1])
+            this.operation.splice(i - 1, 3, this.operationNumber.toString())
+            break;
+
+          }
+        }
+      }
+      this.storedValue = Number(this.operation[0])
     }
+    else if (this.firstOp) {
+      this.firstOp = false;
+      this.operation.splice(0, 1, value.toString(), next)
+    } else
+      if (!this.operationEnded)
+        this.operation.splice(0, 1, value.toString(), next);
+      else
+        this.operation.push(value.toString(), next);
   }
 
 
   setValue() {
     this.newValue = Number(this.displayNumber.toString());
+    this.operationEnded = false;
   }
+
 
   changeDisplay(value: number) {
     if (this.nextOperation === "=")
@@ -94,22 +133,28 @@ export class CalculatorComponent implements OnInit {
     this.displayNumber = this.displayNumber.toString() + value.toString();
   }
 
+
   changeSmallDisplay(thisOperation: string, newCharType: string) {
     if (this.nextOperation != "=")
       this.smallDisplayNumber = this.previousOperation + thisOperation;
     this.previousOperation = this.smallDisplayNumber;
 
-    if (newCharType === "value") {
+    if (this.operationEnded === true){
+      this.lastChar = this.operation[this.operation.length - 1];
+    }
+    else if (newCharType === "value") {
       this.lastChar = this.smallDisplayNumber.charAt(this.smallDisplayNumber.length - 1);
     } else if (newCharType === "operation") {
       this.lastChar = this.smallDisplayNumber.charAt(this.smallDisplayNumber.length - 2);
     }
   }
 
+
   clearSmallDisplay() {
     this.previousOperation = "";
     this.smallDisplayNumber = "";
   }
+
 
   addDot() {
     if ((!this.displayNumber.includes(".")) && (this.lastChar != "")) {
@@ -117,6 +162,7 @@ export class CalculatorComponent implements OnInit {
       this.changeSmallDisplay(".", "value");
     }
   }
+
 
   allCLear() {
     this.displayNumber = "0";
@@ -126,15 +172,21 @@ export class CalculatorComponent implements OnInit {
     this.nextOperation = "";
     this.lastChar = "";
 
+    this.operation = [""];
+    this.operationNumber = 0;
+
     this.newValue = 0;
     this.storedValue = 0;
 
     this.firstOp = true;
+    this.operationEnded = false;
     this.operationBreak = true;
   }
 
+
   constructor() {
   }
+
 
   ngOnInit(): void {
     this.allCLear();
